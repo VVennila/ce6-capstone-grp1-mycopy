c9990dbabc8d43fbafca47bdec7ccb34db340085
VVennila <nilarajan@gmail.com>
2024-10-15 15:38:02 +0800

    updated backend and main tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
  terraform {
    backend "s3" {
     bucket = "sctp-ce6-tfstate"
-    key    = "ecs-ce6-capstone-grp1.tfstate"
+    key    = "ven-ecs-ce6.tfstate"
     region = "ap-southeast-1"
    }
  }
--- terraform/main.tf
+++ terraform/main.tf
@@ -23,15 +23,15 @@ data "aws_subnets" "public" {
 }
 
 resource "aws_ecr_repository" "ecr" {
-  name         = "${local.prefix}-ecr"
-  force_delete = true
+  name         = "${local.prefix}-ecr"  # ECR name will be unique per environment
+  force_delete = true                    # Allows the repository to be deleted even if it contains images
 }
 
 module "ecs" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "~> 5.9.0"
 
-  cluster_name = "${local.prefix}-ecs"
+  cluster_name = "${local.prefix}-ecs"  # ECS cluster name will be unique per environment
 
   fargate_capacity_providers = {
     FARGATE = {
@@ -42,13 +42,13 @@ module "ecs" {
   }
 
   services = {
-    ven = { # ECS Task and Service
+    ven = { # ECS Task and Service configuration
       cpu    = 512
       memory = 1024
       container_definitions = {
         ven-ecs-container = {
           essential = true
-          image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
+          image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"  # Image name will include the environment
           port_mappings = [
             {
               containerPort = 8080
@@ -59,7 +59,7 @@ module "ecs" {
       }
       assign_public_ip = true
       subnet_ids       = flatten(data.aws_subnets.public.ids)
-      security_group_ids = [module.ecs_sg.security_group_id]
+      security_group_ids = [module.ecs_sg.security_group_id]  # Reference to security group created in the module below
     }
   }
 }
@@ -68,10 +68,10 @@ module "ecs_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 5.1.0"
 
-  name        = "${local.prefix}-ecs-sg"
+  name        = "${local.prefix}-ecs-sg"  # Security Group name will be unique per environment
   vpc_id      = data.aws_vpc.default.id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-8080-tcp"]
-  egress_rules        = ["all-all"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]  # Allow incoming traffic from anywhere
+  ingress_rules       = ["http-8080-tcp"]  # Allow HTTP traffic on port 8080
+  egress_rules        = ["all-all"]  # Allow all outgoing traffic
 }
